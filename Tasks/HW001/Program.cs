0ff01d1af200acf0cf858b5b6b6217b7fa61d8be
«Anton» <hranitel-liks@mail.ru>
2022-08-11 20:01:17 +0700

    add HW001

--- Tasks/HW001/Program.cs
+++ Tasks/HW001/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine("Введите число от 1 до 7");
+int a = int.Parse(Console.ReadLine());
+
+if (a <= 5 && a <= 7)
+{
+    Console.WriteLine("Будни");
+}
+else
+{
+    Console.WriteLine("Выходной");
+}
