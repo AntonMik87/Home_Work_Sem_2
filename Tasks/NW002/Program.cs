0ce89eb8b8fcb9ddda59a2d8015b08a908f946ab
«Anton» <hranitel-liks@mail.ru>
2022-08-11 22:35:00 +0700

    add HW002

---
+++ Tasks/NW002/Program.cs
@@ -0,0 +1,20 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("Введите число A затем B");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+int x = a * a;
+int y = b * b;
+
+Console.WriteLine("Квадрат числа A = " + x);
+
+if (x == b)
+{
+    Console.WriteLine("Числа A являются квадратом числа B: " + x + " = " + b);
+}
+else
+{
+    Console.WriteLine("Число B не является квадратом числа A");
+}
+
+
