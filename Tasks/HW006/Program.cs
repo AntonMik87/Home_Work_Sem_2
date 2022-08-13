57d2051c214a0c77e6928e9adff9bda7100bb22c
«Anton» <hranitel-liks@mail.ru>
2022-08-13 20:01:11 +0700

    add HW006

---
+++ Tasks/HW006/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти сумму чисел от 1 до А
+int n = int.Parse(Console.ReadLine() ?? "0");
+int x = 1;
+while (x <= n)
+{
+    Console.Write(x + " ");
+    x++;
+}
+Console.WriteLine("Cумма чисел = " + n*(n+1)/2 );
