c43ca110f8e9d56f3caa7bb01d2cd8bbc4a07e6c
«Anton» <hranitel-liks@mail.ru>
2022-08-13 19:29:45 +0700

    add HW005

--- Tasks/HW003/Program.cs
+++ Tasks/HW003/Program.cs
@@ -7,26 +7,26 @@ int i = int.Parse(Console.ReadLine() ?? "0");
 
 if (i <= 0)
 {
-    Console.WriteLine("Введите число от 1 до 4");
+    Console.WriteLine("Введите номер от 1 до 4");
 }
 else if (i >= 5)
 {
-    Console.WriteLine("Введите число от 1 до 4");
+    Console.WriteLine("Введите номер от 1 до 4");
 }
 
 if (i == 1)
 {
-    Console.WriteLine("Диапозон I четверти: x > n ; y > n");
+    Console.WriteLine("Диапозон I четверти: x > 0 до n ; y > 0 до n");
 }
 if (i == 2)
 {
-    Console.WriteLine("Диапозон II четверти: x < -n ; y > n");
+    Console.WriteLine("Диапозон II четверти: x < -0 до -n; y > 0 до n");
 }
 if (i == 3)
 {
-    Console.WriteLine("Диапозон III четверти: x < -n ; y < -n");
+    Console.WriteLine("Диапозон III четверти: x < -0 до -n; y < -0 до -n");
 }
 if (i == 4)
 {
-    Console.WriteLine("Диапозон IV четверти: x > n ; y < -n");
+    Console.WriteLine("Диапозон IV четверти: x > 0 до n; y < -0 до -n");
 }
---
+++ Tasks/HW005/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти кубы чисел от 1 до N
+int a = int.Parse(Console.ReadLine() ?? "0");
+int i = 1;
+
+while (i <= a)
+{
+    Console.Write(i*i*i + " " );
+    i++;
+}
