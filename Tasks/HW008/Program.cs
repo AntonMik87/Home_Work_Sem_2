093a667d04b403f1ee7e4dad43979f3ba44e4b04
«Anton» <hranitel-liks@mail.ru>
2022-08-13 21:27:43 +0700

    add HW008

---
+++ Tasks/HW008/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int sum = 0;
+while ( a > 0)
+{
+   sum = sum + a % 10;
+   a = a/10;
+   
+}
+Console.WriteLine(sum);
