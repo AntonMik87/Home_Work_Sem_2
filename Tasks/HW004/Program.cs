4d964177b41d17f6b569cfa68f77a0eea5ec6e51
«Anton» <hranitel-liks@mail.ru>
2022-08-14 20:19:36 +0700

    add HW004

--- Tasks/HW004/Program.cs
+++ Tasks/HW004/Program.cs
@@ -4,10 +4,10 @@ double Distance2D(int xa, int ya, int xb, int yb)
 {
     return Math.Sqrt(Math.Pow((xa - xb), 2) + Math.Pow((ya - yb), 2));
 }
-Console.WriteLine(Distance2D(6, 6, 4, 4));
+Console.WriteLine(Distance2D(0, 1, 2, -2));
 
 double Distance3D(int xa, int ya, int za, int xb, int yb, int zb)
 {
-    return Math.Sqrt(Math.Pow((xa - xb), 2) + Math.Pow((ya - yb), 2) + Math.Pow((za - zb), 2));
+    return Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2) + Math.Pow((zb - za), 2));
 }
-Console.WriteLine(Distance3D(6, 6, 4, 4, 7, 7));
+Console.WriteLine(Distance3D(-1, 2, 3, 6, 2, -2));
