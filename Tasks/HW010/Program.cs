4155c1b2e3e36f3f9e5b58265d8419520acd1aff
«Anton» <hranitel-liks@mail.ru>
2022-08-14 17:24:31 +0700

    add HW010

---
+++ Tasks/HW010/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру       // "\t"
+int a = int.Parse(Console.ReadLine() ?? "0");
+int i = 1;
+int ch;
+while (i <= a)
+{
+    ch = (i * i * i);
+    i++;
+    if (ch % 2 == 0)
+    {
+        Console.Write(ch + " ");
+        
+    }
+}
+
